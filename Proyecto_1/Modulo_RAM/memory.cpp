46e54eb437e65e70aeae30cb652971814e4484e6
Jean Paul Jimenez <jpjimenez12@gmail.com>
2020-07-14 05:48:33 -0600

    Update

--- Proyecto_1/Modulo_RAM/memory.cpp
+++ Proyecto_1/Modulo_RAM/memory.cpp
@@ -4,12 +4,18 @@
 #define DATA_WIDTH    32
 #define ADDR_WIDTH    15
 #define BLOCK_WIDTH   3
+#define DATA_MASK     4
 
-SC_MODULE (ram_sp_ar_aw) {
+SC_MODULE (ram) {
   sc_in    <sc_uint<ADDR_WIDTH> > address ;
+  sc_in    <sc_uint<BLOCK_WIDTH> > block  ;
   sc_in    <bool>                 cs      ;
+  sc_in    <bool>                 ras     ;
+  sc_in    <bool>                 cas     ;
   sc_in    <bool>                 we      ;
-  sc_in    <bool>                 oe      ;
+  sc_in    <bool>                 odt     ;
+  sc_in    <bool>                 reset   ;
+  sc_in    <sc_uint<DATA_MASK> > data_mask;
   sc_in    <sc_uint<DATA_WIDTH> > data_in ;
   sc_out   <sc_uint<DATA_WIDTH> > data_out;
 
@@ -17,7 +23,7 @@ SC_MODULE (ram_sp_ar_aw) {
   sc_uint <DATA_WIDTH> mem [RAM_DEPTH];
 
   // Memory Write Block 
-  Write Operation : When we = 1, cs = 1
+  // Write Operation : When we = 1, cs = 1
   void write_mem () {
     if (cs.read() && we.read()) {
       mem[address.read()] = data_in.read();
@@ -32,7 +38,7 @@ SC_MODULE (ram_sp_ar_aw) {
     }
   }
 
-  SC_CTOR(ram_sp_ar_aw) {
+  SC_CTOR(ram) {
     SC_METHOD (read_mem);
     sensitive << address << cs << we << oe;
     SC_METHOD (write_mem);
