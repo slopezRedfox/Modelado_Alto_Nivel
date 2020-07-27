f9db483ecf0c287d4ffb09992d6b640ed2b6cc16
RoRamire24 <robertojose24@hotmail.com>
2020-07-27 00:51:39 -0600

    Delete compile_run_cpu.sh

--- Proyecto_2/CPU/compile_run_cpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo 'Compiling *.c *cpp files'
-rm -rf memory.o
-export SYSTEMC_HOME=/usr/local/systemc-2.3.3/
-export LD_LIBRARY_PATH=$SYSTEMC_HOME/lib-linux64
-g++ -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib-linux64 cpu_tb.cpp cpu.cpp  -lsystemc -lm -o cpu.o
-echo 'Simulation Started'
-./cpu.o
-echo 'Simulation Ended'
