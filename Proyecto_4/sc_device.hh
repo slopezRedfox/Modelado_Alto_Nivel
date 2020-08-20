48661b492f7d593448463768816d594b426a78fb
Santiago Lopez <santiago241996@gmail.com>
2020-08-19 20:11:30 -0600

    Proyecto4 primera prueba

---
+++ Proyecto_4/sc_device.hh
@@ -0,0 +1,69 @@
+#ifndef __SIM_SC_DEVICE_HH__
+#define __SIM_SC_DEVICE_HH__
+
+#include <tlm_utils/peq_with_cb_and_phase.h>
+#include <tlm_utils/simple_target_socket.h>
+
+#include <iostream>
+#include <systemc>
+#include <tlm>
+
+using namespace sc_core;
+using namespace std;
+
+struct Device: sc_module
+{
+    /** TLM interface socket: */
+    tlm_utils::simple_target_socket<Device> socket;
+
+    /** TLM related member variables: */
+    tlm::tlm_generic_payload*  transaction_in_progress;
+    sc_event                   target_done_event;
+    bool                       response_in_progress;
+    bool                       debug;
+    tlm::tlm_generic_payload*  next_response_pending;
+    tlm::tlm_generic_payload*  end_req_pending;
+    tlm_utils::peq_with_cb_and_phase<Device> m_peq;
+
+    /** Storage, may be implemented with a map for large devices */
+    unsigned char *mem;
+
+    Device(sc_core::sc_module_name name,
+        bool debug,
+        unsigned long long int size,
+        unsigned int offset);
+    SC_HAS_PROCESS(Device);
+
+    /** TLM interface functions */
+    virtual void b_transport(tlm::tlm_generic_payload& trans,
+                             sc_time& delay);
+    virtual unsigned int transport_dbg(tlm::tlm_generic_payload& trans);
+    virtual tlm::tlm_sync_enum nb_transport_fw(
+                tlm::tlm_generic_payload& trans,
+                tlm::tlm_phase& phase,
+                sc_time& delay);
+
+    /** Callback of Payload Event Queue: */
+    void peq_cb(tlm::tlm_generic_payload& trans,
+                const tlm::tlm_phase& phase);
+
+    /** Helping function common to b_transport and nb_transport */
+    void execute_transaction(tlm::tlm_generic_payload& trans);
+
+    /** Helping functions and processes: */
+    void send_end_req(tlm::tlm_generic_payload& trans);
+    void send_response(tlm::tlm_generic_payload& trans);
+
+    /** Method process that runs on target_done_event */
+    void execute_transaction_process();
+
+    /** Helping function that checks if a requested address is with range */
+    void check_address(unsigned long long int addr);
+
+    /** Helping Variables **/
+    unsigned long long int size;
+    unsigned offset;
+};
+
+#endif //__SIM_SC_DEVICE_HH__
+
---
+++ Proyecto_4/sc_router.hh
@@ -0,0 +1,53 @@
+#ifndef __SIM_SC_ROUTER_HH__
+#define __SIM_SC_ROUTER_HH__
+
+#include <tlm_utils/peq_with_cb_and_phase.h>
+#include <tlm_utils/simple_target_socket.h>
+#include "tlm_utils/simple_initiator_socket.h"
+
+#include <iostream>
+#include <systemc>
+#include <tlm>
+
+using namespace sc_core;
+using namespace sc_dt;
+using namespace std;
+
+template<unsigned int N_TARGETS>
+struct Router: sc_module
+{
+    /** TLM interface socket: */
+    tlm_utils::simple_target_socket<Router> socket;
+     
+    // *********************************************
+    // Use tagged sockets to be able to distinguish incoming backward path calls
+    // *********************************************
+
+    tlm_utils::simple_initiator_socket_tagged<Router>* initiator_socket[N_TARGETS];
+
+    /** TLM related member variables: */
+    bool                       debug;
+        
+    SC_HAS_PROCESS(Router);
+    Router(sc_core::sc_module_name name,
+        bool debug);
+   
+
+    /** TLM interface functions */
+    virtual void b_transport(tlm::tlm_generic_payload& trans,
+                             sc_time& delay);
+    virtual unsigned int transport_dbg(tlm::tlm_generic_payload& trans);
+    virtual tlm::tlm_sync_enum nb_transport_fw(
+                tlm::tlm_generic_payload& trans,
+                tlm::tlm_phase& phase,
+                sc_time& delay);
+
+    virtual tlm::tlm_sync_enum nb_transport_bw(
+                int id,
+                tlm::tlm_generic_payload& trans,
+                tlm::tlm_phase& phase, 
+                sc_time& delay );
+};
+
+#endif //__SIM_SC_ROUTER_HH__
+
