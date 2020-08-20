5e3ab5b8a45e4f23ac8c325229896c0f64e65d01
slopezRedfox <santiago241996@gmail.com>
2020-08-20 02:16:53 +0000

    agregado main.cc

---
+++ Proyecto_4/main.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2015, University of Kaiserslautern
+ * Copyright (c) 2016, Dresden University of Technology (TU Dresden)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Matthias Jung
+ *          Christian Menard
+ *          Abdul Mutaal Ahmad
+ */
+
+/**
+ * @file
+ *
+ *  Example top level file for SystemC-TLM integration with C++-only
+ *  instantiation.
+ *
+ */
+
+#include <systemc>
+#include <tlm>
+
+#include "cli_parser.hh"
+#include "report_handler.hh"
+#include "sc_target.hh"
+#include "sc_device.hh"
+#include "sc_router.hh"
+#include "sc_router.cc"
+#include "sim_control.hh"
+#include "slave_transactor.hh"
+#include "stats.hh"
+
+int
+sc_main(int argc, char **argv)
+{
+    CliParser parser;
+    parser.parse(argc, argv);
+
+    sc_core::sc_report_handler::set_handler(reportHandler);
+
+    Gem5SystemC::Gem5SimControl sim_control("gem5",
+                                           parser.getConfigFile(),
+                                           parser.getSimulationEnd(),
+                                           parser.getDebugFlags());
+
+    unsigned long long int memorySize = 512*1024*1024ULL;
+
+    Gem5SystemC::Gem5SlaveTransactor transactor("transactor", "transactor");
+    Target memory("memory",
+                  parser.getVerboseFlag(),
+                  memorySize,
+                  parser.getMemoryOffset());
+
+    Device device("device",
+                  parser.getVerboseFlag(),
+                  memorySize,
+                  parser.getMemoryOffset());
+    
+    Router<2> router("router",
+                  parser.getVerboseFlag());
+
+    router.socket.bind(transactor.socket);
+    router.initiator_socket[0]->bind(memory.socket);
+    router.initiator_socket[1]->bind(device.socket);
+    transactor.sim_control.bind(sim_control);
+   
+    SC_REPORT_INFO("sc_main", "Start of Simulation");
+
+    sc_core::sc_start();
+
+    SC_REPORT_INFO("sc_main", "End of Simulation");
+
+    CxxConfig::statsDump();
+
+    return EXIT_SUCCESS;
+}
