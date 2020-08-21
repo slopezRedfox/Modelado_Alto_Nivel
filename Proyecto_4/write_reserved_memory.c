e07933de8c34ccac8ea9b6d704c91477b80345ad
Santiago Lopez <santiago241996@gmail.com>
2020-08-21 14:21:58 -0600

    update

--- Proyecto_4/write_reserved_memory.c
+++ Proyecto_4/write_reserved_memory.c
@@ -11,22 +11,22 @@
     int main() {
             int fd;
             int *reserved_memory;
-            int *buffer = 1;
+            int buffer = 1;
+            
 
             fd = open("/dev/mem", O_RDWR | O_SYNC);
             /* Returns a pointer to the 4GB point in /dev/mem - the start of my reserved memory. Only mapping 4096 bytes. */
             reserved_memory = (int *) mmap(0, 4, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, RESERVED_MEMORY_OFFSET);
 
             printf("Print\n");
-            printf("data: %s\n", buffer);
-            printf("Addr: %x\n", *buffer);
+            printf("data: %x\n", buffer);
 
             if (reserved_memory == MAP_FAILED) {
                     printf("Failed to creating mapping.\n");
                     printf("ERRNO: %s\n", strerror(errno));
                     return -1;
             }
-            sprintf(reserved_memory, "%s", buffer);
+            sprintf(reserved_memory, "%d", &buffer);
             printf("Print reseved\n");
             printf("data  reseved: %x \n", reserved_memory);
             printf("Addr  reseved: %x \n", *reserved_memory);
