701ab7781b0eebe65e0f1cb2592affe78203990f
Santiago Lopez <santiago241996@gmail.com>
2020-08-24 09:09:27 -0600

    9:09 am try to sed 2 comands

--- Proyecto_4/write_reserved_memory.c
+++ Proyecto_4/write_reserved_memory.c
@@ -1,34 +1,45 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 
-    #include <errno.h>
-    #include <fcntl.h>
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
-    #include <sys/mman.h>
+#define RESERVED_MEMORY_OFFSET_0  0x1ff00000     /* Offset */
+#define RESERVED_MEMORY_OFFSET_4  0x1ff00004     /* Offset */
 
-    #define RESERVED_MEMORY_OFFSET  0x1ff00000     /* Offset */
+int main() {
+    int fd;
+    int *reserved_memory_1;
+    int *reserved_memory_2;
 
-    int main() {
-            int fd;
-            int *reserved_memory;
-            int buffer;
-            buffer = 0xA;
+    int buffer_1 = 0xA;
+    int buffer_2 = 0xDCBA;
 
-            fd = open("/dev/mem", O_RDWR | O_SYNC);
-            /* Returns a pointer to the 4GB point in /dev/mem - the start of my reserved memory. Only mapping 4096 bytes. */
-            reserved_memory  = (int *) mmap(0, 1, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, RESERVED_MEMORY_OFFSET);
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
 
-            printf("data: %x\n", buffer);
-            if (reserved_memory == MAP_FAILED) {
-                    printf("Failed to creating mapping.\n");
-                    printf("ERRNO: %s\n", strerror(errno));
-                    return -1;
-            }
-	    memcpy(reserved_memory, &buffer, 4);
+    /* Returns a pointer to the 4GB point in /dev/mem - the start of my reserved memory. Only mapping 4096 bytes. */
+    reserved_memory_1 = (int *) mmap(0, 4, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, RESERVED_MEMORY_OFFSET_0);
+    reserved_memory_2 = (int *) mmap(0, 4, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, RESERVED_MEMORY_OFFSET_4);
 
-            //sprintf(reserved_memory, "%d", buffer);
-            //printf("\n Print reseved\n\n");
-            //printf("data  reseved: %x \n", aux);
-            return 0;
+    printf("data: %x\n", buffer_1);
+    printf("data: %x\n", buffer_2);
+
+    //============================================
+    if (reserved_memory_1 == MAP_FAILED) {
+        printf("Failed to creating mapping.\n");
+        printf("ERRNO: %s\n", strerror(errno));
+        return -1;
     }
+    memcpy(reserved_memory_1, &buffer_1, 4);
+        
+    //============================================
+    if (reserved_memory_2 == MAP_FAILED) {
+        printf("Failed to creating mapping.\n");
+        printf("ERRNO: %s\n", strerror(errno));
+        return -1;
+    }
+    memcpy(reserved_memory_2, &buffer_1, 4);
+    return 0;
+}
 
