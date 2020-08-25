7973ac92d719509e077d2f21f06a6fc84b21d293
Jean Paul Jimenez <jpjimenez12@gmail.com>
2020-08-25 16:48:08 -0600

    Add header and test file v1

---
+++ Proyecto_5/Poligon.h
@@ -0,0 +1,16 @@
+#ifndef _POLIGON_H_
+#define _POLIGON_H_
+
+#include <iostream>
+#include <fstream>
+
+#define YSIZE 256
+#define XSIZE 256
+
+using namespace std;
+
+void Scan_Line(int);
+
+int InOut_Test(int, int, int);
+
+#endif
